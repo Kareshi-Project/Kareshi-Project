ed968c4e46f42de9b25dc47c7bac2ffdb3ef5c4b
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-03-14 12:13:13 -0300

    Create Main.hx

---
+++ source/Main.hx
@@ -0,0 +1,34 @@
+package;
+
+import openfl.display.Sprite;
+import flixel.FlxGame;
+import play.PlayState;
+
+class Main extends Sprite
+{
+    public function new()
+    {
+        super();
+
+        var gameWidth:Int = 1280;
+        var gameHeight:Int = 720;
+        var initialState:Class<Dynamic> = PlayState;
+        var zoom:Float = -1;
+        var framerate:Int = 60;
+        var skipSplash:Bool = true;
+        var startFullscreen:Bool = false;
+
+        var game = new FlxGame(
+            gameWidth,
+            gameHeight,
+            initialState,
+            zoom,
+            framerate,
+            framerate,
+            skipSplash,
+            startFullscreen
+        );
+
+        addChild(game);
+    }
+}
