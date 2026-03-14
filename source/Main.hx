270b79e428e153b75ab097130b943201f7e84ec5
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-03-14 12:25:59 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -12,21 +12,19 @@ class Main extends Sprite
 
         var gameWidth:Int = 1280;
         var gameHeight:Int = 720;
-        var initialState = PlayState;
-        var zoom:Float = -1;
-        var framerate:Int = 60;
-        var skipSplash:Bool = true;
-        var startFullscreen:Bool = false;
+        var zoom:Float = 1;
+        var updateFPS:Int = 60;
+        var drawFPS:Int = 60;
 
         var game = new FlxGame(
             gameWidth,
             gameHeight,
-            initialState,
+            PlayState,
             zoom,
-            framerate,
-            framerate,
-            skipSplash,
-            startFullscreen
+            updateFPS,
+            drawFPS,
+            true,
+            false
         );
 
         addChild(game);
